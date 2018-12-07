eb422896b7a402f4f7b5ee81e73fd9f3de78b622
Terry <terrancesmith98@gmail.com>
2018-12-07 13:52:11 -0500

    Added CodeNode.ActiveDirectory nuget package for user management

---
+++ Controllers/BaseController.cs
@@ -0,0 +1,20 @@
+﻿using CodeNode.ActiveDirectory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Finch_Inventory.Controllers
+{
+    public class BaseController : Controller
+    {
+        // GET: Base
+        public ActionResult Index()
+        {
+            var manager = new ActiveDirectoryManager();
+            var currWinUser = manager.GetCurrentWindowUser();
+            return View();
+        }
+    }
+}
